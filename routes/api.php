6440ab0dee024707f4bdd7802f5a69fe4c1cda5b
IamShaDoW666 <www.gaming2018@gmail.com>
2022-12-28 17:57:41 +0530

    fixed group error

--- routes/api.php
+++ routes/api.php
@@ -65,27 +65,11 @@ Route::post('/notify-customer', function (Request $request) {
         'number' => $request->number,
         'message' => $request->message
     ];
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'http://65.2.191.198/send-message',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => json_encode($data),
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
-        ),
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-    echo $response;
+    $payload = [
+        'form_params' => $data
+    ];
+    $client = new \GuzzleHttp\Client(['http_errors' => false]);
+    $client->request('POST', 'http://65.2.191.198/send-message', $payload);
 });
 
 Route::post('/save-number', function (Request $request) {
@@ -110,6 +94,7 @@ Route::post('/save-number', function (Request $request) {
             'name' => $request->group
         ]);
     }
+
     $alreadyExists = $tag->contacts->where('number', $request->number)->first();
     if (!$alreadyExists) {
         Contact::create([
